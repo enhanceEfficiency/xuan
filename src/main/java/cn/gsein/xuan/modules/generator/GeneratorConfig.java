c3b7878d97c154e1f708a8da2f7c7ede09af809e
lhing17 <lhing17@163.com>
2020-07-04 21:46:47 +0800

    feat: 准备着手代码生成器。

---
+++ src/main/java/cn/gsein/xuan/modules/generator/GeneratorConfig.java
@@ -0,0 +1,21 @@
+package cn.gsein.xuan.modules.generator;
+
+/**
+ * 代码生成器配置的实体
+ *
+ * @author G. Seinfeld
+ * @since 2020/07/04
+ */
+public class GeneratorConfig {
+
+    /**
+     * 模块名，如system
+     */
+    private String moduleName;
+
+    /**
+     * 当前工程所在路径
+     */
+    private String projectPath;
+
+}
---
+++ src/main/java/cn/gsein/xuan/modules/generator/GeneratorMain.java
@@ -0,0 +1,13 @@
+package cn.gsein.xuan.modules.generator;
+
+/**
+ * 代码生成器入口类
+ *
+ * @author G. Seinfeld
+ * @since 2020/07/04
+ */
+public class GeneratorMain {
+    public static void main(String[] args) {
+
+    }
+}
