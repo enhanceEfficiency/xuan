241b3066f5a0d6c951fae5dc9340545b40848c71
lianghao <lhing17@163.com>
2020-07-14 14:11:10 +0800

    feat: 角色按条件查询。

--- src/main/java/cn/gsein/xuan/modules/system/dao/RoleDao.java
+++ src/main/java/cn/gsein/xuan/modules/system/dao/RoleDao.java
@@ -2,7 +2,12 @@ package cn.gsein.xuan.modules.system.dao;
 
 import cn.gsein.xuan.common.dao.BaseDao;
 import cn.gsein.xuan.modules.system.entity.Role;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -14,4 +19,20 @@ import java.util.Optional;
 public interface RoleDao extends BaseDao<Role> {
 
     Optional<Role> findByNameAndDeletedIsFalse(String name);
+
+    @Override
+    default Specification<Role> getSpecification(Role role) {
+        List<Predicate> predicates = new ArrayList<>();
+        return (root, criteriaQuery, criteriaBuilder) -> {
+            if (role != null) {
+                // 是否有查询条件
+                if (!StringUtils.isEmpty(role.getName())) {
+                    predicates.add(criteriaBuilder.like(root.get("name"), "%" + role.getName() + "%"));
+                }
+            }
+            predicates.add(criteriaBuilder.equal(root.get("deleted"), 0));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+
+    }
 }
